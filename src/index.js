db41db112277c9c7a178d0cb002fe4f009fa9456
lorna gathumbi <nyawiralornah@gmail.com>
2024-10-06 15:15:08 +0300

    tasklister

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+let form =document.querySelector('#create-task-form')
+form.addEventListener('submit',(e) => {
+  e.preventDefault();
+  let newTask = document.querySelector('#new-task-description').value;
+  buildToDo(newTask);
+  form.reset();
+})
+
+function buildToDo(taskDescription) {
+let li = document.createElement('li');
+li.textContent = taskDescription;
+let btn = document.createElement('button');
+btn.addEventListener('click', handleDelete);
+btn.textContent = 'delete';
+li.appendChild(btn)
+console.log(li)
+document.querySelector('#tasks').appendChild(li)
+}
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
 });
